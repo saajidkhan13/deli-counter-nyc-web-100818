7a627a6b5e7a67b565d8333cb474d3831938ece3
Saajid Khan <saajid.khan13@stjohns.edu>
2018-09-24 17:20:01 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+katz_deli= []
+
+def line(katz_deli)
+  if katz_deli.length == 0 
+    puts "The line is currently empty."
+  else
+    currentLine= ["The line is currently:"]
+    katz_deli.each_with_index do |name, index|
+      currentLine.push("#{index + 1}. #{name}")
+    end 
+    puts currentLine.join(" ")
+  end
+end 
