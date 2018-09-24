6fe9022e04478b3603fcb6b48afd5cc3512d07c1
Saajid Khan <saajid.khan13@stjohns.edu>
2018-09-24 17:21:24 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,3 +12,8 @@ def line(katz_deli)
     puts currentLine.join(" ")
   end
 end 
+
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end 
